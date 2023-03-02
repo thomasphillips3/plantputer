e9c9e0d6cf64ef030a3f9189eadb7116aeac5410
Thomas Phillips <thomasphillips3@gmail.com>
2023-03-02 04:45:17 +0000

    convert to fahrenheit and start db

--- plantputer.py
+++ plantputer.py
@@ -6,6 +6,7 @@ import Adafruit_DHT
 from gpiozero import DigitalInputDevice
 import xlwt
 from xlwt import Workbook
+import sqlite3
 
 d0_input = DigitalInputDevice(17)
 wb = Workbook()
@@ -13,9 +14,19 @@ data_sheet = wb.add_sheet('data')
 row = 1
 col = 1
 
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+def write_to_database(value):
+    conn = sqlite3.connect('plantputer.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS plantputer(temp TEXT, humidity TEXT)''')
+    c.execute("INSERT INTO plantputer VALUES('','')")
+
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(11,4)
-    print('Temp: {0:0.1f} C Humidity: {1:0.1f} %'.format(temperature, humidity))
+    fahrenheit = celsius_to_fahrenheit(temperature)
+    print('Temp: {0:0.1f} F Humidity: {1:0.1f} %'.format(fahrenheit, humidity))
     data_sheet.write(row, col, humidity)
     col+=1
     data_sheet.write(row, col, temperature)
